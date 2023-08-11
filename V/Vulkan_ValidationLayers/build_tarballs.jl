13bacac2add80ddcb6483e6514f78ea08cc5791e
serenity4 <cedric.bel@hotmail.fr>
2023-08-11 17:12:10 +0200

    Add prototyping lines

--- V/Vulkan_ValidationLayers/build_tarballs.jl
+++ V/Vulkan_ValidationLayers/build_tarballs.jl
@@ -2,13 +2,13 @@ using BinaryBuilder
 
 name = "Vulkan_Validation_Layers"
 
-version = v"1.3.243"
+version = v"1.3.250"
 source = "https://github.com/KhronosGroup/Vulkan-ValidationLayers.git"
-commit = "188c2c436349f9a9fd3a8561edbd054d6c77f7a6" # April 30th, 2023
+commit = "d6c1e5a5a6229b52c20c643ff626a948d4850865" # June 29th, 2023
 
 sources = [
     GitSource(source, commit),
-    DirectorySource("./patches"),
+    # DirectorySource("./patches"),
     # For tests.
     # GitSource("https://github.com/KhronosGroup/Vulkan-Loader", "71254bedeef4b9e94f7a51ec0fe6e4134dbfcfec"),
 ]
@@ -58,16 +58,16 @@ install_license LICENSE.txt
 
 platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
-    Platform("x86_64", "macos"),
+    # Platform("x86_64", "macos"),
     # TODO: Build on Windows.
     # Might want to take some inspiration from https://github.com/google/gfbuild-swiftshader/blob/master/build.sh
     # Platform("x86_64", "windows"),
 ]
 
-platforms = expand_cxxstring_abis(platforms)
+# platforms = expand_cxxstring_abis(platforms)
 
 products = [
-    FileProduct("explicit_layer.d"), # at $install_dir/share/vulkan/, except for Windows, which is at $install_dir/bin/
+    FileProduct("explicit_layer.d", :explicit_layer), # at $install_dir/share/vulkan/, except for Windows, which is at $install_dir/bin/
     LibraryProduct("libvulkan", :libvulkan), # at $install_dir/lib/, except for Windows, which is at $install_dir/bin/
 ]
 
@@ -85,6 +85,7 @@ build_dependencies = [
     Dependency("Xorg_libX11_jll"),
     Dependency("Xorg_libxcb_jll"),
     # Is it needed?
+    Dependency("Xorg_xproto_jll"),
     # Dependency("Xorg_xcb_proto_jll"),
 
     # Windows-specific dependencies
@@ -100,6 +101,7 @@ test_dependencies = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [build_dependencies]#; test_dependencies]
+# dependencies = [build_dependencies; test_dependencies]
+dependencies = build_dependencies
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"11")
